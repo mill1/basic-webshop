eeda532537f42bab04f40b536647588462dba2ed
Emiel Nijhuis <enijhuis@outlook.com>
2020-04-10 15:53:00 +0200

    mock commit

--- Earless.WebApi.Test/Mocks/MockFactory.cs
+++ Earless.WebApi.Test/Mocks/MockFactory.cs
@@ -6,6 +6,7 @@ using Earless.WebApi.Models;
 
 namespace Earless.WebApi.Test.Mocks
 {
+    // mock edit
     public class MockFactory
     {
         private static List<ProductCategory> productCategories;
